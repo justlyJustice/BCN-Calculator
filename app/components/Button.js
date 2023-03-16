3b95c6af4c2ce2d865cda93aa929e923d677d064
Justice Johnson <justlyjohn198@gmail.com>
2023-03-16 15:41:53 +0100

    wrap-up for project

---
+++ app/components/Button.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Button = ({ text, symbol }) => {
+  return (
+    <View>
+      <Text>{text}</Text>
+    </View>
+  );
+};
+
+export default Button;
